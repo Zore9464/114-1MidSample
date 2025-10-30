0693a0be9d17282ad5a9bec53f302125439599cc
Zore9464 <aka0903912930@gmail.com>
2025-10-30 21:13:55 +0800

    c updateStock finished

--- q2.js
+++ q2.js
@@ -1,3 +1,6 @@
+
+
+
 const products = [
   { name: "keyboard", stock: 25 },
   { name: "mouse", stock: 5 },
@@ -12,3 +15,23 @@ function getLowStock(products) {
 }
 
 console.log(getLowStock(products)); // ["mouse", "monitor"]
+
+function updateStock(products, updates) {
+  // 依照 updates 更新對應商品的庫存
+  for (let product of products) {
+    if (updates.hasOwnProperty(product.name)) {
+      product.stock = updates[product.name];
+    }
+  }
+
+  // 輸出結果
+  for (let product of products) {
+    console.log(`${product.name} 的庫存： ${product.stock}`);
+  }
+
+  // 若要讓 console.log(updateStock(...)) 有輸出，可以回傳字串或陣列
+  return products;
+}
+
+  const updates = { mouse: 15, monitor: 20 };
+  updateStock(products, updates);
